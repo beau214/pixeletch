d38f063903fd24ee62a33e716c9468711246e157
Beau Williams <39930920+beau214@users.noreply.github.com>
2022-12-20 16:04:09 -0600

    Update scripts.js

--- scripts.js
+++ scripts.js
@@ -1,20 +1,26 @@
-let create = document.getElementById("makeGrid");
+let etchMode = document.getElementById("etchMode");
 let board = document.getElementById("container");
 let num = document.getElementById("gridSize");
 let bar = document.getElementById("size");
 let pixelMode = document.getElementById("pixelMode");
 let colorPicker = document.getElementById("colorPicker");
 let color = colorPicker.value;
+let newBoard = document.getElementById("generate");
 
 colorPicker.addEventListener("change", function(){
     color=colorPicker.value;
+    num.style.color = color
     console.log(color);
 })
 
 start();
 
+function colorChange(){
+    this.style.backgroundColor = color;
+}
+
 function start(){
-    board.replaceChildren();
+    
     gridSize = 16;
     num.innerHTML = bar.value + "x" + bar.value;
     board.style.gridTemplateColumns = 'repeat(' + gridSize + ', 1fr)';
@@ -23,14 +29,14 @@ function start(){
         let cell = document.createElement("div");
         cell.setAttribute("class", "cell");
         board.appendChild(cell);
-        cell.addEventListener("mouseover", function(e){
-            this.style.backgroundColor = color;
-        })
-
+    }
+    let cells = document.getElementsByClassName("cell");
+    for(let i = 0; i < cells.length;i++){
+        cells[i].addEventListener("mouseover", colorChange)
     }
 }
 
-create.addEventListener("click", function(){
+newBoard.addEventListener("click", function(){
     board.replaceChildren();
     let gridSize = document.getElementById("size").value;
     
@@ -40,39 +46,30 @@ create.addEventListener("click", function(){
         let cell = document.createElement("div");
         cell.setAttribute("class", "cell");
         board.appendChild(cell);
-        cell.addEventListener("mouseover", function(e){
-            this.style.backgroundColor = color;
-        })
-
     }
-   
- 
-  
-})
-
-pixelMode.addEventListener("click", function(){
-    board.replaceChildren();
-    let gridSize = document.getElementById("size").value;
-    
-    board.style.gridTemplateColumns = 'repeat(' + gridSize + ', 1fr)';
-    let total = gridSize*gridSize;
-    for(let i=0; i < total; i++){
-        let cell = document.createElement("div");
-        cell.setAttribute("class", "cell");
-        board.appendChild(cell);
-        cell.addEventListener("click", function(e){
-            this.style.backgroundColor = color;
-        })
-
+    let cells = document.getElementsByClassName("cell");
+    for(let i = 0; i < cells.length;i++){
+        cells[i].addEventListener("mouseover", colorChange)
     }
-   
- 
-  
 })
-
-
 
 bar.addEventListener("change", function(){
     num.innerHTML = bar.value + "x" + bar.value;
 })
 
+pixelMode.addEventListener("click", function(){
+    let cells = document.getElementsByClassName("cell");
+    for(let i = 0; i < cells.length; i++){
+        cells[i].removeEventListener("mouseover", colorChange)
+        cells[i].addEventListener("click", colorChange);
+    }
+})
+
+etchMode.addEventListener("click", function(){
+    let cells = document.getElementsByClassName("cell");
+    for(let i = 0; i < cells.length; i++){
+        cells[i].removeEventListener("click", colorChange)
+        cells[i].addEventListener("mouseover", colorChange);
+    }
+    
+})
